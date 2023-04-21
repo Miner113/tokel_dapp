4eba0332d7389006a728a7f37ad4d26bd20a09f0
Alexander Khudyakov <89903767+Miner113@users.noreply.github.com>
2023-04-21 11:44:48 +0700

    Update links.ts

--- src/util/links.ts
+++ src/util/links.ts
@@ -11,10 +11,10 @@ export default {
     TKLTEST: 'https://explorer.komodoplatform.com:10000/tkltest/api/',
   },
   discord: 'https://discord.gg/MHxJZVFkqa',
-  website: 'https://tokel.io',
-  websiteRoadmap: 'https://tokel.io/roadmap',
+  website: 'https://nftx.pw',
+  websiteRoadmap: 'https://nftx.pw',
   githubIssue:
-    'https://github.com/TokelPlatform/tokel_app/issues/new?assignees=&labels=bug&template=1-Bug_report.md',
-  devEmail: 'mailto:support@tokel.io',
+    'https://github.com/nftx-sergei',
+  devEmail: 'mailto:support@nftx.pw',
   security: 'https://hackernoon.com/best-practices-for-key-security-for-your-crypto-wallets',
 };
